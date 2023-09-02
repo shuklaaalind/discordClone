d880eb072ed4091cc12706f0ad15e93a61b46258
Aalind Shukla <aalind.shukla@quantel.ai>
2023-09-03 05:22:07 +0530

    shadcn integration

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/Saurav/Documents/GitHub/discordClone/app/layout\":[\"static/media/2aaf0723e720e8b9-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/Saurav/Documents/GitHub/discordClone/app/layout\":[\"static/media/cdbcea0e589a6ff7-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "10cec76f5267da4e"; }
+/******/ 		__webpack_require__.h = function() { return "efbf811f40f7f40b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
